e5c450831d1776c1ad8239c2e312299ac4796139
Gabriel Valentoni Guelfi <gabriel.valguelfi@gmail.com>
2025-01-29 19:04:10 -0300

    minor fix

--- components/Modal.vue
+++ components/Modal.vue
@@ -20,7 +20,7 @@
           <div :class="`col-12 q-py-xs-xs q-px-md-xs ${dynamicColClass}`">
             <q-btn class="full-width" dense label="Fechar" color="grey-8" icon="close" v-close-popup></q-btn>
           </div>
-          <div v-if="!HideActions" v-for="action in Actions" :key="action.label"
+          <div v-show="!HideActions" v-for="action in Actions" :key="action.label"
             :class="`col-12 q-py-xs-xs q-px-md-xs ${dynamicColClass}`">
             <q-btn dense class="full-width" v-show="!action.hide" :label="action.label" :color="action.color"
               :icon="action.icon" @click="action.fn"></q-btn>
