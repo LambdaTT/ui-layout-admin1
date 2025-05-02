66072956eae474a8462ea3d40ec088db814a0e32
Mkopaes <murilo.paes191@gmail.com>
2025-05-02 12:04:07 -0300

    Update: Card Layout

--- components/Card.vue
+++ components/Card.vue
@@ -2,15 +2,15 @@
   <q-card>
     <q-card-section :class="`${dense ? 'q-pa-sm' : 'q-pa-lg'} bg-${style.headerBgColor} text-${style.headerTextColor}`">
       <!--Card Header-->
-      <div class="row">
-        <div class="col-12 col-md-4 q-mb-sm q-mb-md-none" style="display: flex; align-items: center;">
+      <div class="row items-center q-gutter-y-sm">
+        <div :class="`col-12 ${forceMobile? '':'col-md-4 q-mb-md-none'}`" style="display: flex; align-items: center;">
           <span class="text-h6" :style="`display: flex; align-items: center; gap: ${dense ? '1px' : '4px'}`">
             <q-icon v-if="!!Icon" :name="Icon" :size="dense ? 'xs' : 'md'"></q-icon>
             <span v-if="!!Icon">&nbsp;</span>
             {{ Title }}
           </span>
         </div>
-        <div class="col-12 col-md-8 text-right">
+        <div v-if="(`actions` in $slots)" :class="`col-12 text-right ${forceMobile? '':'col-md-8'}`">
           <slot name="actions"></slot>
         </div>
       </div>
@@ -41,6 +41,7 @@ export default {
     HeaderBgColor: String,
     HeaderTextColor: String,
     dense: Boolean,
+    forceMobile: Boolean
   },
 
   data() {
